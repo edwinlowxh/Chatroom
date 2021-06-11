be7fd25cbf6a221354ae15193efa3ca965c13d56
edwinlowxh <edwin.lowxh@gmail.com>
2021-06-11 21:34:46 +0800

    Search for users frontend

--- mysite/Chatroom/static/search_users_friends.js
+++ mysite/Chatroom/static/search_users_friends.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(()=> {
   var pathname = window.location.pathname;
   if (pathname == "/Chatroom/friends"){
     document.title = "Friends";
@@ -6,4 +6,9 @@ $(document).ready(function() {
   else{
     document.title = "Users";
   }
+
+  $(".list-group-item").click(()=> {
+    $(".user-profile").find("#name").html("{{Users.1.first_name}}")
+    $(".user-profile").find("#name").load("search_users_friends.html");
+  });
 })
