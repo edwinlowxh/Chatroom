a1e6b23db3bee5f40870ba5d19f23fdf49a81300
edwinlowxh <edwin.lowxh@gmail.com>
2021-07-06 21:24:41 +0800

    Added differentiation b/w own message and other's message

--- mysite/Chatroom/consumers.py
+++ mysite/Chatroom/consumers.py
@@ -26,21 +26,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user_id = text_data_json["user_id"]
+        first_name = text_data_json["first_name"]
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user_id': user_id,
+                'first_name': first_name
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        user_id = event["user_id"]
+        first_name = event["first_name"]
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user_id': user_id,
+            'first_name': first_name
         }))
--- mysite/Chatroom/static/interface.js
+++ mysite/Chatroom/static/interface.js
@@ -1,6 +1,9 @@
 $(document).ready(()=> {
   //Get the chatroom name
   var groupname;
+  var user_id = $("#user_id").html();
+  var first_name = $("#first_name").html().slice(1, -1); //Slice to remove inverted commas
+
   $("#interface-container .nav-link").on("click", e=>{
     groupname = $(e.currentTarget).html();
   })
@@ -17,7 +20,9 @@ $(document).ready(()=> {
   //Receiving messages
   chatSocket.onmessage = e=>{
     const data = JSON.parse(e.data);
-    $("#message-box").append(data.message);
+    if (data.user_id != user_id){
+      $("#message-box").append(data.message);
+    }
   }
 
   //Close socket
@@ -25,6 +30,13 @@ $(document).ready(()=> {
     console.error("Chat socket closed unexpectedly");
   };
 
+  //Listen for enter key press
+  $("#message-input").on("keydown", e=>{
+    if (e.keyCode === 13){
+      e.preventDefault();
+      $("#send-button").trigger("click");
+    }
+  })
 
   $("#message-input").on("keyup", e=>{
     $("#send-button").on("click", a=>{
@@ -36,9 +48,24 @@ $(document).ready(()=> {
       //Send message
       const message = $("#message-input").val();
       chatSocket.send(JSON.stringify({
-        'message': message
+        'message': message,
+        'user_id': user_id,
+        'first_name': first_name
       }));
       $("#message-input").val("");
+
+      //Append message to chat container
+      $("#message-box").append('<div class="d-flex justify-content-start mb-4 message-container">\
+                                  <div class="circle" id="profile-pic">\
+                                    <span class="profile-pic-initial">A</span>\
+                                  </div>\
+                                  <div class="ml-3 message-container-message">' + message +
+                                    '<div class="d-flex justify-content-end message-information">\
+                                      <span id=>9:12 AM, Today</span>\
+                                      <span class="material-icons" id="delete-icon">delete</span>\
+                                    </div>\
+                                  </div>\
+                                </div>');
       a.stopImmediatePropagation();
     })
   })
